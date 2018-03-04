2473a62280f83f79e6214f2098b1f9df7e19cbc7
Kira Shatulova <kira.shatulova@gmail.com>
2018-03-04 12:28:44 +0300

    task1

---
+++ index.js
@@ -0,0 +1,34 @@
+let fs = require('fs');
+let path = require('path');
+
+function getFiles (dir, files_) {
+  files_ = files_ || [];
+  let files = fs.readdirSync(dir);
+  for (let i in files) {
+    let name = dir + '/' + files[i];
+    if (fs.statSync(name).isDirectory()) {
+      getFiles(name, files_);
+    } else {
+      files_.push(name);
+    }
+  }
+  return files_;
+}
+
+function sortFiles (files) {
+  if (!fs.existsSync('./result')) {
+    fs.mkdirSync('./result');
+  }
+  files.forEach(function (item) {
+    let name = path.basename(item);
+    let dirName = './result/' + name.slice(0, 1);
+    if (!fs.existsSync(dirName)) {
+      fs.mkdirSync(dirName);
+    }
+    let readable = fs.createReadStream(item);
+    let writeable = fs.createWriteStream(dirName + '/' + name);
+    readable.pipe(writeable);
+  });
+}
+
+sortFiles(getFiles('./foxes-img'));
